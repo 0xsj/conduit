ead25390621af13d1127ef15e482f6e3469ebf93
sjtommylee <sjtommylee@gmail.com>
2024-03-04 11:12:02 -0800

    STEP A2: scaffolding, deps

---
+++ app/src/main/java/com/example/conduit/database/AppDatabase.java
@@ -0,0 +1,5 @@
+package com.example.conduit.database;
+
+
+public class AppDatabase {
+}
---
+++ app/src/main/java/com/example/conduit/entities/Vacation.java
@@ -0,0 +1,8 @@
+package com.example.conduit.entities;
+
+import androidx.room.Entity;
+
+@Entity(tableName = "vacations")
+public class Vacation {
+
+}
