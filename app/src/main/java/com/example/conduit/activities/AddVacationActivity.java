50c444ae620fde26ac7bc147acdbd39abf0be992
sjtommylee <sjtommylee@gmail.com>
2024-03-04 12:43:08 -0800

    STEP C: added GUI for required layouts

--- app/src/main/java/com/example/conduit/activities/AddVacationActivity.java
+++ app/src/main/java/com/example/conduit/activities/AddVacationActivity.java
@@ -1,6 +1,22 @@
 package com.example.conduit.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.conduit.database.AppDatabase;
+
 public class AddVacationActivity extends AppCompatActivity {
+
+    private AppDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize the database
+        db = AppDatabase.getDatabase(getApplicationContext());
+    }
+
+
 }
--- app/src/main/java/com/example/conduit/database/AppDatabase.java
+++ app/src/main/java/com/example/conduit/database/AppDatabase.java
@@ -1,11 +1,32 @@
 package com.example.conduit.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.conduit.dao.ExcursionDao;
+import com.example.conduit.dao.VacationDao;
 import com.example.conduit.entities.Excursion;
 import com.example.conduit.entities.Vacation;
 
 @Database(entities = {Vacation.class, Excursion.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+    public abstract VacationDao vacationDao();
+    public abstract ExcursionDao excursionDao();
+
+    private static volatile AppDatabase DB_INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (DB_INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (DB_INSTANCE == null) {
+                    DB_INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "app_database").build();
+                }
+            }
+        }
+        return DB_INSTANCE;
+    }
+
 }
