16154319b2b7dceea215060d13e324aa91f907bd
sjtommylee <sjtommylee@gmail.com>
2024-03-04 13:50:08 -0800

    STEP C2: updates to UI, fixes in resource linking

--- app/src/main/java/com/example/conduit/activities/AddVacationActivity.java
+++ app/src/main/java/com/example/conduit/activities/AddVacationActivity.java
@@ -4,15 +4,31 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.conduit.R;
 import com.example.conduit.database.AppDatabase;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 public class AddVacationActivity extends AppCompatActivity {
+    private EditText editTitle, editHotel, editStartDate, editEndDate;
 
     private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.vacation_add);
+        editTitle = findViewById(R.id.editTitle);
+        editHotel = findViewById(R.id.editHotel);
+        editStartDate = findViewById(R.id.editStartDate);
+        editEndDate = findViewById(R.id.editEndDate);
+
+        Button saveVacationButton = findViewById(R.id.saveVacation);
+        Button viewVacationButton = findViewById(R.id.viewVacations);
+
+
+
 
         // Initialize the database
         db = AppDatabase.getDatabase(getApplicationContext());
--- app/src/main/java/com/example/conduit/activities/UpdateExcursionActivity.java
+++ app/src/main/java/com/example/conduit/activities/UpdateExcursionActivity.java
@@ -3,4 +3,5 @@ package com.example.conduit.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateExcursionActivity extends AppCompatActivity {
+
 }
